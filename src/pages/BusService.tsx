e113ef2c8ee7cd5486555ab6e6b85b998c90295f
JoabChua <chuaweeliang91@gmail.com>
2021-10-27 12:25:03 +0800

    update bus service implementation and styling

--- src/pages/BusService.tsx
+++ src/pages/BusService.tsx
@@ -6,7 +6,6 @@ import {
   IonMenuButton,
   IonTitle,
   IonContent,
-  IonList,
   IonItem,
   IonLoading,
   IonAlert,
@@ -16,25 +15,31 @@ import {
 import { search } from "ionicons/icons";
 import { useCallback, useEffect, useState } from "react";
 import { BUS_SERVICE_API, LTA_ACCESSS_KEY } from "../configs/bus.config";
+import { BusServiceModel, BusServiceResponseModel } from "../models/bus.model";
 import "./BusService.scss";
+import { Virtuoso } from "react-virtuoso";
+import { Http } from "@capacitor-community/http";
+import { isPlatform } from "@ionic/react";
 
 const compare = (a: BusServiceModel, b: BusServiceModel) => {
-  const reA = /[^a-zA-Z]/g;
-  const reN = /[^0-9]/g;
-  const aA = a.ServiceNo.replace(reA, "");
-  const bA = b.ServiceNo.replace(reA, "");
-  if (aA === bA) {
-    const aN = parseInt(a.ServiceNo.replace(reN, ""), 10);
-    const bN = parseInt(b.ServiceNo.replace(reN, ""), 10);
-    return aN === bN ? 0 : aN > bN ? 1 : -1;
-  } else {
-    return aA > bA ? 1 : -1;
-  }
+  // const reA = /[^a-zA-Z]/g;
+  // const reN = /[^0-9]/g;
+  // const aA = a.ServiceNo.replace(reA, "");
+  // const bA = b.ServiceNo.replace(reA, "");
+  // if (aA === bA) {
+  //   const aN = parseInt(a.ServiceNo.replace(reN, ""), 10);
+  //   const bN = parseInt(b.ServiceNo.replace(reN, ""), 10);
+  //   return aN === bN ? 0 : aN > bN ? 1 : -1;
+  // } else {
+  //   return aA > bA ? 1 : -1;
+  // }
 
-  // return a.ServiceNo.localeCompare(b.ServiceNo);
+  return a.ServiceNo.localeCompare(b.ServiceNo);
 };
 
-const BusServices: React.FC = () => {
+const BusServices: React.FC<{ setBus(bus: BusServiceModel): void }> = ({
+  setBus,
+}) => {
   const [busServices, setBusServices] = useState<BusServiceModel[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<any>();
@@ -42,22 +47,26 @@ const BusServices: React.FC = () => {
   const fetchBusStops = useCallback(async () => {
     setIsLoading(true);
     try {
-      const endpoint1 = fetch(
-        `https://cors-anywhere.herokuapp.com/${BUS_SERVICE_API}`,
-        { headers: LTA_ACCESSS_KEY },
-      );
-      const endpoint2 = fetch(
-        `https://cors-anywhere.herokuapp.com/${BUS_SERVICE_API}?$skip=500`,
-        { headers: LTA_ACCESSS_KEY },
-      );
+      const endpoint1 = Http.get({
+        url: `${
+          isPlatform("mobileweb") ? "https://cors-anywhere.herokuapp.com/" : ""
+        }${BUS_SERVICE_API}`,
+        headers: LTA_ACCESSS_KEY,
+      });
+      const endpoint2 = Http.get({
+        url: `${
+          isPlatform("mobileweb") ? "https://cors-anywhere.herokuapp.com/" : ""
+        }${BUS_SERVICE_API}?$skip=500`,
+        headers: LTA_ACCESSS_KEY,
+      });
 
       const [res1, res2] = await Promise.all([endpoint2, endpoint1]);
-      if (!res1.ok || !res2.ok) {
+      if (res1.status !== 200 || res2.status !== 200) {
         throw new Error("Something went wrong!");
       }
 
-      const [data1, data2]: [BusResponseModel, BusResponseModel] =
-        await Promise.all([res1.json(), res2.json()]);
+      const [data1, data2]: [BusServiceResponseModel, BusServiceResponseModel] =
+        [res1.data, res2.data];
       const key = "ServiceNo";
       const data = [
         ...new Map(
@@ -108,21 +117,31 @@ const BusServices: React.FC = () => {
           buttons={["OK"]}
         />
         {busServices.length > 0 && (
-          <IonList>
-            {busServices.map((bus) => {
+          <Virtuoso
+            style={{ height: "100%" }}
+            data={busServices}
+            totalCount={busServices.length}
+            itemContent={(index, bus: BusServiceModel) => {
               const customClass = `ops ${bus.Operator.toLowerCase()}`;
+              const routeLink = `/busservices/${bus.ServiceNo}`;
               return (
-                <IonItem key={bus.ServiceNo} detail>
+                <IonItem
+                  key={bus.ServiceNo}
+                  detail
+                  routerLink={routeLink}
+                  routerDirection="forward"
+                  onClick={() => setBus(bus)}
+                >
                   <div className="item">
                     <div className="service">{bus.ServiceNo}</div>
                     <div className={customClass}>{bus.Operator}</div>
                   </div>
                 </IonItem>
               );
-            })}
-          </IonList>
+            }}
+          />
         )}
-        {busServices.length === 0 && (
+        {!isLoading && busServices.length === 0 && (
           <div className="refresh-wrapper">
             <IonButton shape="round">Refresh Page</IonButton>
           </div>
@@ -133,22 +152,3 @@ const BusServices: React.FC = () => {
 };
 
 export default BusServices;
-
-interface BusResponseModel {
-  "odata.metadata": string;
-  value: BusServiceModel[];
-}
-
-interface BusServiceModel {
-  ServiceNo: string;
-  Operator: string;
-  Direction: number;
-  Category: string;
-  OriginCode: string;
-  DestinationCode: string;
-  AM_Peak_Freq: string;
-  AM_Offpeak_Freq: string;
-  PM_Peak_Freq: string;
-  PM_Offpeak_Freq: string;
-  LoopDesc: string;
-}
