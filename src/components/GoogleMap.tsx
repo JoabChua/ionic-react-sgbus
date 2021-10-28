c4da8c858206865b02133a88917ca5ad722dc394
JoabChua <chuaweeliang91@gmail.com>
2021-10-28 22:50:16 +0800

    refactor duplicated code into helper functions. added infoWindow to link to bus stop from map.

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -1,8 +1,10 @@
 import GoogleMapReact from "google-map-react";
 import { useState } from "react";
+import { useHistory } from "react-router";
 import { BKUP_MAP_KEY } from "../configs/bus.config";
 import { BusStopModel } from "../models/bus.model";
 import { GoogleMapStartingPoint } from "../pages/BusArrival";
+import "./GoogleMap.scss";
 
 const filterBusStops = (
   busStops: BusStopModel[],
@@ -27,25 +29,94 @@ const setOriginMarker = (
   center: { lat: number; lng: number },
   map: google.maps.Map,
 ) => {
-  setCenterMarker(() => [
-    new google.maps.Marker({
-      position: center,
-      map,
-      icon: {
-        url: "https://img.icons8.com/dusk/64/000000/marker.png",
-        scaledSize: new google.maps.Size(32, 32),
-      },
-    }),
-  ]);
+  setCenterMarker(
+    () =>
+      new google.maps.Marker({
+        position: center,
+        map,
+        icon: {
+          url: "assets/img/marker.png",
+          scaledSize: new google.maps.Size(32, 32),
+        },
+      }),
+  );
+};
+
+const infoWindow = (bs: BusStopModel) => {
+  return `
+      <a
+        style="font-size: 16px; font-weight: 800; color: grey"
+        id="goTo">
+        ${bs.Description}
+      </a>
+  `;
+};
+
+const filterBusListAndUpdateBusMarkers = (
+  data: BusStopModel[],
+  coord: GoogleMapStartingPoint,
+  setBusStopList: any,
+  map: google.maps.Map,
+  setBusStopMarkers: any,
+  setBusStop: any,
+  history: any,
+) => {
+  const tempBusStopMarkers: google.maps.Marker[] = [];
+  const tempInfoWindows: google.maps.InfoWindow[] = [];
+  let filteredBusStops = filterBusStops(data, coord);
+  setBusStopList(filteredBusStops);
+
+  filteredBusStops.forEach((bs: BusStopModel) => {
+    tempBusStopMarkers.push(
+      new google.maps.Marker({
+        position: { lat: bs.Latitude, lng: bs.Longitude },
+        map,
+        icon: {
+          url: "assets/img/busstop.png",
+          scaledSize: new google.maps.Size(24, 24),
+        },
+      }),
+    );
+
+    tempInfoWindows.push(
+      new google.maps.InfoWindow({
+        content: infoWindow(bs),
+      }),
+    );
+  });
+
+  let currentlyOpenInfoWindow: number | null;
+
+  tempBusStopMarkers.forEach((marker, index) => {
+    marker.addListener("click", () => {
+      tempInfoWindows.forEach((iw) => iw.close());
+      if (currentlyOpenInfoWindow !== index) {
+        tempInfoWindows[index].open(map, marker);
+        tempInfoWindows[index].addListener("domready", () => {
+          document.getElementById("goTo")?.addEventListener("click", () => {
+            setBusStop(filteredBusStops[index]);
+            history.push("/busarrival/" + filteredBusStops[index].BusStopCode);
+          });
+        });
+        currentlyOpenInfoWindow = index;
+      } else {
+        currentlyOpenInfoWindow = null;
+      }
+    });
+  });
+
+  setBusStopMarkers(tempBusStopMarkers);
 };
 
 const GoogleMap: React.FC<{
   coord: GoogleMapStartingPoint;
   setBusStopList(filterbs: BusStopModel[]): void;
   setCoord(coord: GoogleMapStartingPoint): void;
-}> = ({ coord, setBusStopList, setCoord }) => {
+  setBusStop(busStop: BusStopModel): void;
+}> = ({ coord, setBusStopList, setCoord, setBusStop }) => {
+  const history = useHistory();
   const [gMap, setGMap] = useState<google.maps.Map>();
-  const [centerMarker, setCenterMarker] = useState<google.maps.Marker[]>([]);
+  const [centerMarker, setCenterMarker] = useState<google.maps.Marker>();
   const [busStopMarkers, setBusStopMarkers] = useState<google.maps.Marker[]>(
     [],
   );
@@ -53,7 +124,7 @@ const GoogleMap: React.FC<{
     [],
   );
 
-  const renderMarkers = (map: google.maps.Map) => {
+  const renderMarkers = (map: google.maps.Map, maps: any) => {
     setGMap(map);
 
     let data: BusStopModel[] = require("../services/busstop.json");
@@ -62,60 +133,44 @@ const GoogleMap: React.FC<{
       return [...new Map(data.map((item) => [item[key], item])).values()];
     });
 
-    const tempBusStopMarkers: google.maps.Marker[] = [];
-    let filteredBusStops = filterBusStops(data, coord);
-    setBusStopList(filteredBusStops);
-
-    filteredBusStops.forEach((bs: BusStopModel) => {
-      tempBusStopMarkers.push(
-        new google.maps.Marker({
-          position: { lat: bs.Latitude, lng: bs.Longitude },
-          map,
-          icon: {
-            url: "https://img.icons8.com/dusk/64/000000/bus--v2.png",
-            scaledSize: new google.maps.Size(24, 24),
-          },
-        }),
-      );
-    });
-    setBusStopMarkers(tempBusStopMarkers);
-
     setOriginMarker(setCenterMarker, coord.center, map);
+    filterBusListAndUpdateBusMarkers(
+      data,
+      coord,
+      setBusStopList,
+      map,
+      setBusStopMarkers,
+      setBusStop,
+      history,
+    );
   };
 
-  const updateMarkers = (ev: google.maps.Map) => {
+  const onDragEndHandler = (ev: google.maps.Map) => {
+    const newCenter = {
+      lat: ev.getCenter()!.lat(),
+      lng: ev.getCenter()!.lng(),
+    };
     const newCoord = {
-      center: { lat: ev.getCenter()!.lat(), lng: ev.getCenter()!.lng() },
+      center: newCenter,
       zoom: ev.getZoom()!,
     };
     setCoord(newCoord);
 
-    centerMarker.forEach((marker) => marker.setMap(null));
+    if (centerMarker) {
+      centerMarker.setMap(null);
+    }
     busStopMarkers.forEach((marker) => marker.setMap(null));
 
-    setOriginMarker(
-      setCenterMarker,
-      { lat: ev.getCenter()!.lat(), lng: ev.getCenter()!.lng() },
+    setOriginMarker(setCenterMarker, newCenter, gMap!);
+    filterBusListAndUpdateBusMarkers(
+      unfilteredBusStops,
+      newCoord,
+      setBusStopList,
       gMap!,
+      setBusStopMarkers,
+      setBusStop,
+      history,
     );
-
-    const tempBusStopMarkers: google.maps.Marker[] = [];
-    let filteredBusStops = filterBusStops(unfilteredBusStops, newCoord);
-    setBusStopList(filteredBusStops);
-
-    filteredBusStops.forEach((bs: BusStopModel) => {
-      tempBusStopMarkers.push(
-        new google.maps.Marker({
-          position: { lat: bs.Latitude, lng: bs.Longitude },
-          map: gMap,
-          icon: {
-            url: "https://img.icons8.com/dusk/64/000000/bus--v2.png",
-            scaledSize: new google.maps.Size(24, 24),
-          },
-        }),
-      );
-    });
-    setBusStopMarkers(tempBusStopMarkers);
   };
 
   return (
@@ -136,11 +191,11 @@ const GoogleMap: React.FC<{
           },
         ],
       }}
-      onDragEnd={($event) => updateMarkers($event)}
+      onDragEnd={($event) => onDragEndHandler($event)}
       center={coord.center}
       zoom={coord.zoom}
       yesIWantToUseGoogleMapApiInternals
-      onGoogleApiLoaded={({ map }) => renderMarkers(map)}
+      onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
     ></GoogleMapReact>
   );
 };
