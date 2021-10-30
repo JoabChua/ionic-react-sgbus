26193eacad87a48677d215e7a3958f9f3cefe5b2
JoabChua <chuaweeliang91@gmail.com>
2021-10-30 23:24:22 +0800

    update to show no bus stop found when surrounding does not have any bus stop nearby center

--- src/components/BusStopList.tsx
+++ src/components/BusStopList.tsx
@@ -10,27 +10,34 @@ const BusStopList: React.FC<{
   );
 
   return (
-    <IonList>
-      {newList.map((busStop) => {
-        const routeLink = `/busarrival/${busStop.BusStopCode}`;
-        return (
-          <IonItem
-            key={busStop.BusStopCode + busStop.Description}
-            routerLink={routeLink}
-            routerDirection="forward"
-            onClick={() => setBusStop(busStop)}
-          >
-            <div className="stop">
-              <div className="left">
-                <div className="desc">{busStop.Description}</div>
-                <div className="road">{busStop.RoadName}</div>
-              </div>
-              <div className="code">{busStop.BusStopCode}</div>
-            </div>
-          </IonItem>
-        );
-      })}
-    </IonList>
+    <div>
+      {newList.length > 0 && (
+        <IonList>
+          {newList.map((busStop) => {
+            const routeLink = `/busarrival/${busStop.BusStopCode}`;
+            return (
+              <IonItem
+                key={busStop.BusStopCode + busStop.Description}
+                routerLink={routeLink}
+                routerDirection="forward"
+                onClick={() => setBusStop(busStop)}
+              >
+                <div className="stop">
+                  <div className="left">
+                    <div className="desc">{busStop.Description}</div>
+                    <div className="road">{busStop.RoadName}</div>
+                  </div>
+                  <div className="code">{busStop.BusStopCode}</div>
+                </div>
+              </IonItem>
+            );
+          })}
+        </IonList>
+      )}
+      {newList.length === 0 && (
+        <div className="no-info">No Bus Stop Available</div>
+      )}
+    </div>
   );
 };
 
